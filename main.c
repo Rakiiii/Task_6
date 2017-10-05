f5bc026871335e28935ad1a40a3ad8d08581bcae
Rakiiii <sudaru2@gmail.com>
2017-10-05 22:23:08 +0300

    refactored

--- main.c
+++ main.c
@@ -1,51 +1,41 @@
 #include <windef.h>
 #include "Include_and_constants.h"
 
-int Length(int Number)
-{
-    int Length = 0;
-    while(Number > 0)
-    {
-        Length++;
-        Number = Number / 2;
-    }
-    return Length;
-}
 
 int Translation()
 {
-    int DecNumber = 0;
-    system("cls");
-    printf("Print number for translation_ ");
-    DecNumber = In();
-    printf("Your Number in Binary form is\n");
-    if( DecNumber < 0 )printf("1");
-    else printf("0");
-    BinaryNumbers( DecNumber , BinaryLength(DecNumber) );
-    printf("\n");
-    return 0;
+    int DecNumber = 0 ;
+    system( "cls" ) ;
+    printf( "Print number for translation_ " ) ;
+    DecNumber = In() ; //читаем число для перевода
+    printf( "Your Number in Binary form is\n" ) ;
+    if( DecNumber < 0 ) printf( "1" ) ; //разруливаем знак числа для доп кода
+    else printf( "0" ) ;
+    BinaryNumbers( DecNumber , BinaryLength( DecNumber ) ); //выводим значение в двоичной форме
+    printf( "\n" ) ;
+    return 0 ;
 }
 
 
 
 int main()
 {
-    while(TRUE)
+    while(TRUE) //меню
     {
-        system("cls");
-        printf("1.Translate Dec to Bin number\n2.Exit\n");
-        char Choose = getch();
-        switch(Choose)
+        system( "cls" ) ;
+        printf( "1.Translate Dec to Bin number\n2.Exit\n" ) ;
+        char Choose = getch() ;
+        switch( Choose )
         {
             case '1' :
-                Translation();
-                printf("Press any button to restart the programme_");
-                getch();
-                break;
+                Translation() ;
+                printf( "Press any button to restart the programme_" ) ;
+                getch() ;
+                break ;
             case '2' :
-                return 0;
+                return 0 ;
             default:
-                break;
+                break ;
         }
     }
 
